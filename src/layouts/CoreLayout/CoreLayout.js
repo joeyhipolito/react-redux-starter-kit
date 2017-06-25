3b3b48e8e76ef75fdea96800a1cd8ce870d642d2
Joey Hipolito <hi@joeyhipolito.me>
2017-06-25 10:40:17 +0800

    create todo example

--- src/layouts/CoreLayout/CoreLayout.js
+++ src/layouts/CoreLayout/CoreLayout.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Header from 'components/Header'
 import './CoreLayout.scss'
 import 'styles/core.scss'
 
 export const CoreLayout = ({ children }) => (
   <div className='core-layout'>
-    <Header />
     <div className='core-layout__viewport'>
       {children}
     </div>
